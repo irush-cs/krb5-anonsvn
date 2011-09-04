845b2b1494189983e689e18c49a473e864d64823
Yair Yarom <irush@cs.huji.ac.il>
2011-09-04 11:06:44 +0300

    rebase fixes

--- src/plugins/preauth/otp/otp_main.c
+++ src/plugins/preauth/otp/otp_main.c
@@ -101,11 +101,13 @@
 #include "otp.h"
 #include "m_basicauth.h"
 #include "m_ykclient.h"
+#include "m_pam.h"
 
 /* Configured OTP methods.  */
 struct otp_method otp_methods[] = {
     {"basicauth", otp_basicauth_server_init, 0, NULL, NULL},
     {"ykclient", otp_ykclient_server_init, 0, NULL, NULL},
+    {"pam", otp_pam_server_init, 0, NULL, NULL},
     {NULL, NULL, 0, NULL, NULL}
 };
 
